c70bad565a43b68eecb5e2eec42bea9f7c63a90b
brice-morgat <brice.morgat@epitech.eu>
2022-07-27 16:52:32 +0200

    Init project

--- src/main/java/com/nnk/springboot/services/IBidService.java
+++ src/main/java/com/nnk/springboot/services/IBidService.java
@@ -1,5 +1,21 @@
 package com.nnk.springboot.services;
 
-public interface BidService {
-    
+import com.nnk.springboot.domain.BidList;
+
+public interface IBidService {
+    /**
+     * Save a bid list.
+     *
+     * @param bidList The bid list to be saved.
+     * @return The BidList object that was saved.
+     */
+    public BidList saveBidList(BidList bidList);
+
+    /**
+     * Delete a bid list.
+     *
+     * @param bidList The bidList object to be deleted.
+     * @return The BidList object that was deleted.
+     */
+    public void deleteBidList(BidList bidList);
 }
--- src/main/java/com/nnk/springboot/services/impl/BidServiceImpl.java
+++ src/main/java/com/nnk/springboot/services/impl/BidServiceImpl.java
@@ -1,2 +1,24 @@
-package com.nnk.springboot.services.impl;public class BidServiceImpl {
+package com.nnk.springboot.services.impl;
+
+import com.nnk.springboot.domain.BidList;
+import com.nnk.springboot.repositories.BidListRepository;
+import com.nnk.springboot.services.IBidService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BidServiceImpl implements IBidService {
+
+    @Autowired
+    BidListRepository bidListRepository;
+
+    @Override
+    public BidList saveBidList(BidList bidList) {
+        return bidListRepository.save(bidList);
+    }
+
+    @Override
+    public void deleteBidList(BidList bidList) {
+        bidListRepository.delete(bidList);
+    }
 }
